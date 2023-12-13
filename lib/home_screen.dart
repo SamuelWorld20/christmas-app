c752f703cc0c2780269d83d9d4711b77b03fe0fd
Samuel_Oluwaseyi <ejalonibuoluwaseyi@gmail.com>
2023-12-13 03:21:09 +0000

    Welcome screen

---
+++ lib/home_screen.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _fadeAnimation;
+
+  final _formKey = GlobalKey<FormState>();
+  String _name = "";
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController =
+        AnimationController(duration: Duration(seconds: 1), vsync: this);
+    _fadeAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+        CurvedAnimation(parent: _animationController, curve: Curves.easeIn));
+    _animationController.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(20.0),
+          child: FadeTransition(
+            opacity: _fadeAnimation,
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    "assets/images/cover-image.png",
+                    width: double.infinity,
+                  ),
+                  const SizedBox(height: 10.0),
+                  Text(
+                    "CHRISTMAS GIFTS",
+                    style: TextStyle(
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 15.0),
+                  Container(
+                    alignment: Alignment.center,
+                    child: Text(
+                      "generate random quotes to help you raise your holiday spirit",
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.normal,
+                        color: Color(0x8a2e2b2b),
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  const SizedBox(height: 20.0),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: "Enter your name:",
+                      floatingLabelStyle: TextStyle(
+                        color: Color(0xffc63026),
+                      ),
+                      border: OutlineInputBorder(),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Color(0xffc63026),
+                        ),
+                      ),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Please enter your name";
+                      }
+                      return null;
+                    },
+                    onSaved: (newValue) => _name = newValue!,
+                  ),
+                  const SizedBox(height: 20.0),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        _formKey.currentState!.save();
+                        Navigator.pushNamed(context, "/quote",
+                            arguments: _name);
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: Color(0xffc63026),
+                      shape: StadiumBorder(),
+                      padding: EdgeInsets.symmetric(
+                          horizontal: 30.0, vertical: 30.0),
+                    ),
+                    child: Text(
+                      "Let's Celebrate Christmas!",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+}
