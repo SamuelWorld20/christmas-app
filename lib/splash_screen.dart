94379aa4e852f37c00f9b4a63d8709bf308c3d47
Samuel_Oluwaseyi <ejalonibuoluwaseyi@gmail.com>
2023-12-14 00:14:22 +0000

    increase splash screen time

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -13,7 +13,7 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
     Timer(
-      Duration(seconds: 3),
+      Duration(seconds: 8),
       () {
         Navigator.pushReplacement(
           context,
